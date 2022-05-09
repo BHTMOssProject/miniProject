1aa8ac1a725a6b5c5d421273d951fc39868cbc2d
Tamin Lee <taemin5161@gmail.com>
2022-05-09 21:26:51 +0900

    modify design

--- manager.c
+++ manager.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include <stdio.h>
 #include "manager.h"
 
 // 메뉴바
@@ -7,12 +7,13 @@ int selectMenu()
     int menu;
 
     printf("\n한동 친구 찾기 - 나의 친구가 되어줘!\n");
+    printf("=====================================\n");
     printf("0. 종료\n");
     printf("1. User 등록\n");
     printf("2. 커뮤니티 확인\n");
     printf("3. User 업데이트\n");
     printf("4. User 삭제\n");
-    printf("=> 선택할 메뉴 : ");
+    printf("\n=> 선택할 메뉴 : ");
     scanf("%d", &menu);
 
     return menu;
--- person.c
+++ person.c
@@ -6,6 +6,7 @@ void listPerson(Person *p, int count)
     int user_cnt = 0;
     char gender[10];
 
+    printf("\n※ 한동 커뮤니티\n");
     for (int i = 0; i < count; i++) {
         if (p[i].age == -1) continue;
         if (p[i].gender) 
@@ -13,28 +14,31 @@ void listPerson(Person *p, int count)
         else
             strcpy(gender, "남자");
         user_cnt++;
-        printf("-----------------------------\n");
+        printf("------------------------------------------------------\n");
         printf("*** %d번 User ***\n", i+1);
         printf("이름 : %s | 나이 : %d | 성별 : %s | MBTI : %s\n", p[i].name, p[i].age, gender, p[i].mbti);
     }
-    printf("-----------------------------\n");
-    printf("	현재 총 User : %d명\n", user_cnt);
-    printf("-----------------------------\n");
+    printf("------------------------------------------------------\n");
+    printf("=> 현재 총 User : %d명\n", user_cnt);
+    printf("------------------------------------------------------\n");
 }
 
 int createPerson(Person *p, int count){
 
+    printf("\n※ User 등록\n");
+    printf("--------------------------\n");
     printf("이름은? ");
     scanf("%s", p[count].name);
     getchar();
     printf("나이는? ");
     scanf("%d", &p[count].age);
-    printf("성별은?(남자:0, 여자:1) ");
+    printf("성별은? (남자:0, 여자:1) ");
     scanf("%d", &p[count].gender);
     getchar();
-    printf("mbti는? ");
+    printf("MBTI는? ");
     scanf("%s", p[count].mbti);
     getchar();
+    printf("=> 등록 완료\n");
     
     return 1;
 }
@@ -43,6 +47,8 @@ int readPerson(Person *p, int count) // 번호 입력 받고 그 사람 정보 출력
 {
     int printNum;
     char gender[10];
+    printf("\n※ User 검색\n");
+    printf("--------------------------------------\n");
     printf("몇번째 사람을 확인 하시겠습니까? ");
     scanf("%d", &printNum);
     printNum--;
@@ -54,7 +60,7 @@ int readPerson(Person *p, int count) // 번호 입력 받고 그 사람 정보 출력
     printf("사람: %s\n", p[printNum].name);
     printf("나이: %d\n", p[printNum].age);
     printf("성별: %s\n", gender);
-    printf("mbti: %s\n", p[printNum].mbti);
+    printf("MBTI: %s\n", p[printNum].mbti);
     printf("=====================================\n");
 
     return 1;
@@ -68,6 +74,8 @@ int updateData(Person *p, int count)
     
     listPerson(p, count);
 
+    printf("\n※ User 업데이트\n");
+    printf("--------------------------------------\n");
     printf("업데이트할 User 번호 (취소 : 0) : ");
     scanf("%d", &num);
 
@@ -78,7 +86,7 @@ int updateData(Person *p, int count)
     printf("MBTI : ");
     scanf(" %s", p[num-1].mbti);
     
-    printf("=> 업데이트 완료!\n");
+    printf("=> 업데이트 완료\n");
 
     return 1;
 }
@@ -90,6 +98,8 @@ int deletePerson(Person *p, int count)
 
     listPerson(p, count);
 
+    printf("\n※ User 삭제\n");
+    printf("------------------------------------\n");
     printf("삭제할 User 번호 (취소 : 0) : ");
     scanf("%d", &num);
 
